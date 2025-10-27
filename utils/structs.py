53a07d82f8f54bdbc3aba93ed33bca10ddbd23fd
0zean <42080997+0zean@users.noreply.github.com>
2025-10-27 05:01:54 -0400

    Organize structs

--- utils/structs.py
+++ utils/structs.py
@@ -1,4 +1,4 @@
-from ctypes import Structure, c_float, c_uint64
+from ctypes import POINTER, Structure, Union, c_float, c_long, c_uint64, c_ulong
 
 
 class Vec3(Structure):
@@ -11,3 +11,22 @@ class Vec2(Structure):
 
 class C_UTL_VECTOR(Structure):
     _fields_ = [("Count", c_uint64), ("Data", c_uint64)]
+
+
+class MOUSEINPUT(Structure):
+    _fields_ = [
+        ("dx", c_long),
+        ("dy", c_long),
+        ("mouseData", c_ulong),
+        ("dwFlags", c_ulong),
+        ("time", c_ulong),
+        ("dwExtraInfo", POINTER(c_ulong)),
+    ]
+
+
+class INPUT(Structure):
+    class INPUT_UNION(Union):
+        _fields_ = [("mi", MOUSEINPUT)]
+
+    _anonymous_ = ("u",)
+    _fields_ = [("type", c_ulong), ("u", INPUT_UNION)]
